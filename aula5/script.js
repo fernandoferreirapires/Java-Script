c2376e91de9defb6c16d34914925d1121220164e
LuisFernando <zdinhoff@gmail.com>
2024-01-15 21:56:41 -0300

    novo exercicio

---
+++ aula5/script.js
@@ -0,0 +1,25 @@
+var i = 1
+do{
+    console.log('Olá, mundo!!')
+    i++
+} while(i <= 6)
+
+/*
+
+var i = 1
+
+while(i <= 6){
+    console.log('Olá, mundo!!')
+    i++
+}
+*/
+
+
+/*
+console.log('Olá, mundo!!');
+console.log('Olá, mundo!!');
+console.log('Olá, mundo!!');
+console.log('Olá, mundo!!');
+console.log('Olá, mundo!!');
+console.log('Olá, mundo!!');
+*/

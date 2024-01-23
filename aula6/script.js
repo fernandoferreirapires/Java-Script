fa3f0b3671da368b94d446083b2adc7c4e86c8d9
LuisFernando <zdinhoff@gmail.com>
2024-01-23 15:02:45 -0300

    1

--- aula6/script.js
+++ aula6/script.js
@@ -1,6 +1,6 @@
 let num = [0, 5, 2, 1, 4, 3, 7]
 num.push(6) //adicioono elementos na ultima posição
 num[8] = 8
-num.sort() //organiza em ordem crescente
 console.log(`Nosso vetor é o: ${num}`)
+num.sort() //organiza em ordem crescente
 console.log(`Meu vetor tem ${num.length} posições`)
--- aula6/vetores.js
+++ aula6/vetores.js
@@ -4,14 +4,14 @@ console.log (valores)
 valores.sort()
 
 
-/*for(i = 0; i < valores.length; i++){
+for(i = 0; i < valores.length; i++){
     console.log(`A posição ${i} tem o valor ${valores[i]}`)            
-}*/
-
+}
+/*
 for(i in valores){
     console.log('A posição '+ i + ' tem o valor ' + valores[i])
 }
-
+*/
 var pos = valores.indexOf(11)
 if (pos == -1){
     console.log('A posição não foi encontrada!')
